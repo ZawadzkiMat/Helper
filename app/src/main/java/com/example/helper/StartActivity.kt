40f8471d4b5640fba7a5547ae8176ffeca27d668
matza <mat.zawadzki123@gmail.com>
2021-03-01 00:22:41 +0100

    TODO: Complete the prototype of the fence measurements

---
+++ app/src/main/java/com/example/helper/StartActivity.kt
@@ -0,0 +1,20 @@
+package com.example.helper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        finish()
+    }
+}
